4dfa0e66e6d9fcc67b7dc0358d7612414eeca1fe
Denny Wu <denny.wu87@gmail.com>
2017-09-19 21:05:28 -0700

    Adding javascript code for buttons to generate gifs

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -47,3 +47,58 @@ $("#add-animal").on("click", function(event) {
 
 	// Generating buttons for the initial array of animals.
 	createButtons();
+
+// This function will cause an animal button to bring up gifs of that animal from Giphy.
+
+$(".animal").on("click", function(event) {
+
+	// Storing the name of the animal on a given button in a variable.
+
+	var animalType = $(this).attr("data-name")
+
+	// Below are my Giphy API and the URL that will search Giphy for pictures of the animal in animalType.
+
+	var giphyKey = "cb0e018da4bb492b89da79bdc13a1532"
+	var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animalType + "&api_key=" + giphyKey + "&limit=10"
+
+	// This AJAX GET request will generate the gifs.
+
+	$.ajax({
+		url: queryURL,
+		method: GET
+	})
+
+	.done(function(response) {
+
+		// The data returned by the API comes in the form of an array, which will be stored in the "results" variable.
+		var results = response.data
+
+		// Looping over the "results" array and looking for pics with a G or PG rating.
+		for (var i = 0; i < results.length; i++) {
+			
+			if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
+
+				// Creating a div to hold each gif.
+				var gifHolder = $("<div class='animal-gif'>");
+
+				var rating = results[i].rating;
+
+				// Storing the rating of each gif in a variable, which will be added to a paragraph tag.
+				var p = $("<p>").text("Rating: " + rating);
+
+				var animalImage = $("<img>");
+
+				// Creating an image tag and giving it the src of an image returned by the API.
+				animalImage.attr("src", results[i].images.fixed_height.url);
+
+				// Appending the new image and paragraph to the "gifHolder" div.
+				gifHolder.append(p);
+				gifHolder.append(animalImage);
+
+				// Each "gifHolder" div will be prepended to a div on the webpage.
+				$("#gif-container").prepend(gifHolder)
+			}
+		};
+	})
+
+})
